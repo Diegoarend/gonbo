e869187891baab86fd29fb37a910bad92c017cbc
Diegoarend <diegoaw@gmail.com>
2020-07-19 20:53:17 -0300

    FileController e model File criados

--- gonbo/src/routes.js
+++ gonbo/src/routes.js
@@ -7,6 +7,8 @@ import UserController from './app/controllers/UserController';
 
 import SessionController from './app/controllers/SessionController';
 
+import FileController from './app/controllers/FileController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -19,8 +21,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: 'true' });
-});
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
